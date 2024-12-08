456f0d5a0ec1a3d4985bd94191d05026351384f7
Renjiro-kun <renjiro-kun@hotmail.com>
2024-12-08 17:25:45 +0100

    Added message loaing

--- DDScriptWriter/Views/MainView.axaml.cs
+++ DDScriptWriter/Views/MainView.axaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using DDScriptWriter.ViewModels;
 using System.Collections.Generic;
+using Avalonia.Platform.Storage;
 
 namespace DDScriptWriter.Views;
 
@@ -17,13 +18,39 @@ public partial class MainView : UserControl
         (DataContext as MainViewModel).MessagesList.Add(new MessageInternal( 0, "test" ));
 	}
 
-	private void SaveScript_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-	{
-		const string fileName = "script.scr";
 
-		using (var stream = File.Open(fileName, FileMode.Create))
+	private void ReadScriptFile(string filename)
+	{
+		var messages = (DataContext as MainViewModel).MessagesList;
+		messages.Clear();
+
+		using (var stream = File.Open(filename, FileMode.Open))
 		{
-			using(var writer = new BinaryWriter(stream, System.Text.Encoding.UTF8, false))
+			using (var reader = new BinaryReader(stream, System.Text.Encoding.UTF8, false))
+			{
+				byte[] magicNumber = new byte[4];
+				reader.Read(magicNumber, 0, 4);
+				byte scriptType = reader.ReadByte();
+				short messageCount = reader.ReadInt16();
+
+				for (int i = 0; i < messageCount; i++)
+				{
+					short id = reader.ReadInt16();
+					short msgType = reader.ReadByte();
+					string text = reader.ReadString();
+					// Removing '\0' from the string
+					text = text.Remove(text.Length - 1, 1);
+					messages.Add(new MessageInternal(id,text));
+				}
+
+			}
+		}
+	}
+	private void WriteScriptFile(string filename)
+	{
+		using (var stream = File.Open(filename, FileMode.Create))
+		{
+			using (var writer = new BinaryWriter(stream, System.Text.Encoding.UTF8, false))
 			{
 				writer.Write('D');
 				writer.Write('D');
@@ -35,7 +62,7 @@ public partial class MainView : UserControl
 				short messageCount = (short)messages.Count;
 				writer.Write(messageCount);
 
-				foreach(var msg in messages)
+				foreach (var msg in messages)
 				{
 					writer.Write(msg.ID);
 					byte msgType = 0;
@@ -44,6 +71,53 @@ public partial class MainView : UserControl
 					writer.Write(outStr);
 				}
 			}
+		}
+
+	}
+
+	private async void OpenScript_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+	{
+		// Get top level from the current control. Alternatively, you can use Window reference instead.
+		var topLevel = TopLevel.GetTopLevel(this);
+
+		FilePickerFileType scrType = new FilePickerFileType("Script Files")
+		{
+			Patterns = new[] { "*.scr" }
+		};
+
+		// Start async operation to open the dialog.
+		var file = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+		{
+			Title = "Open Script File",
+			FileTypeFilter = new[] { scrType }
+		});
+
+		if (file is not null)
+		{
+			ReadScriptFile(file[0].Path.AbsolutePath);
+		}
+	}
+
+	private async void SaveScript_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+	{
+		// Get top level from the current control. Alternatively, you can use Window reference instead.
+		var topLevel = TopLevel.GetTopLevel(this);
+
+		FilePickerFileType scrType = new FilePickerFileType("Script Files")
+		{
+			Patterns = new[] { "*.scr" }
+		};
+
+		// Start async operation to open the dialog.
+		var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+		{
+			Title = "Save Script File",
+			FileTypeChoices = new[] { scrType }
+		});
+
+		if (file is not null)
+		{
+			WriteScriptFile(file.Path.AbsolutePath);
 		}
 	}
 }
